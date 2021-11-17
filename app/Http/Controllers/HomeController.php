b3aa209f3ab7761789b578d20a69aa929f4a92c5
Demask90 <84012055+Demask90@users.noreply.github.com>
2021-11-17 21:24:22 +0100

    email storage complete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,27 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Lead;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendNewMail;
 
-class HomeController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+class HomeController extends Controller {
 
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+
     public function index() {
         $posts = Post::all();
         return view('guest.home', compact('posts'));
+    }
+
+    public function contact() {
+        return view('guest.contacts');
+    }
+    
+    public function handleContactForm(Request $request){
+        // salviamo a db i dati inseriti nel form di contatto
+        $form_data = $request->all();
+        $new_lead = new Lead();
+        $new_lead->fill($form_data);
+        $new_lead->save();
+        // inviamo la mail all'admin del sito, passando il nuovo oggetto Lead
+        Mail::to('info@boolpress.com')->send(new SendNewMail($new_lead));
+        return redirect()->route('contacts.thank-you');
+    }
+
+    public function thankYou() {
+        return view('guest.thank-you');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/vue-posts', 'PostController@listPostsApi')->name('list-posts-api');
 
 Route::resource('/posts', 'PostController');
 
+Route::get('/contact', 'HomeController@contact')->name('contact');
+Route::post('/contact', 'HomeController@handleContactForm')->name('contacts.send');
+Route::get('/thank-you', 'HomeController@thankYou')->name('contacts.thank-you');
+
 /* Serie di rotte che gestiscono il meccanismo di autenticazione */
 Auth::routes();
 
